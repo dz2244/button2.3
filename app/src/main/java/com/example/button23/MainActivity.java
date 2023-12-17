b5c4476f07244395fb4629b05e27ceec80bd142b
dz2244 <dz2244@bs.amalnet.k12.il>
2023-12-17 22:52:46 +0200

    COMMIT2

--- app/src/main/java/com/example/button23/MainActivity.java
+++ app/src/main/java/com/example/button23/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.button23;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint({"WrongViewCast", "SetTextI18n"})
+    public void btn(View view) {
+        counter++;
+        btn = findViewById(R.id.btn);
+        btn.setText("This is a click number:" + counter);
+
+        
     }
 }
